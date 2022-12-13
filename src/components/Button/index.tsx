2462eda5ff8b6fcfb25cca5b54924dc3915d06e6
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-12 23:09:45 -0300

    feat: extends props for Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,32 @@ import { Component } from '../../types/Component';
 interface Button {
   text: string;
   className?: string;
+  handleClick: () => void;
+  type?: keyof typeof types;
 }
 
+const types = {
+  primary: 'btn btn-active btn-secondary',
+  loading: 'btn loading',
+  error: 'btn btn-error',
+  success: 'btn btn-success loading',
+  disabled: 'btn-disabled btn btn-active btn-secondary',
+};
+
 export const Button: Component<Button> = (props) => {
-  const { text, className } = props;
+  const { text, className, type: propsType, handleClick } = props;
 
-  const defaultClassName = 'btn btn-active btn-secondary';
+  const type = propsType || 'primary';
 
-  return <button className={`${defaultClassName} ${className}`}>{text}</button>;
+  const defaultClassName = types[type];
+
+  return (
+    <button
+      disabled={type === 'disabled'}
+      onClick={handleClick}
+      className={`${defaultClassName} ${className} w-48`}
+    >
+      {text}
+    </button>
+  );
 };
