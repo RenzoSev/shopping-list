045f0dbf4b5f4063877960169db1ff4cf730e619
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-13 17:35:47 -0300

    refactor(hooks): useAvatar attributes

--- src/hooks/useAvatar.ts
+++ src/hooks/useAvatar.ts
@@ -1,8 +1,11 @@
+import { Avatar } from '../server/avatar';
 import { getAvatarFromStorage } from '../utils/getters';
+import { setAvatarInStorage } from '../utils/setters';
 
 export function useAvatar() {
-  const avatar = getAvatarFromStorage();
-  const hasAvatar = Boolean(avatar);
+  const rawAvatar = getAvatarFromStorage();
+  const hasAvatar = Boolean(rawAvatar);
+  const avatar = rawAvatar || ({ img: String(), name: String() } as Avatar);
 
-  return { avatar, hasAvatar };
+  return { avatar, hasAvatar, setAvatar: setAvatarInStorage };
 }
