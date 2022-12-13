213b3a742d6705390db7356477e2f3fc43c1e342
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-12 23:09:59 -0300

    feat: extend TextInput props

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -2,16 +2,20 @@ import { Component } from '../../types/Component';
 
 interface ITextInput {
   placeholder: string;
+  value: string;
+  handleTextInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const TextInput: Component<ITextInput> = (props) => {
-  const { placeholder } = props;
+  const { placeholder, value, handleTextInput } = props;
 
   return (
     <input
       type="text"
+      onChange={handleTextInput}
       placeholder={placeholder}
       className="input input-bordered w-full max-w-xs"
+      value={value}
     />
   );
 };
