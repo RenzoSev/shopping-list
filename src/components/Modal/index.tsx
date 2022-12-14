156f22ceab6f1368a5746eac4632f5badb693942
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-13 21:31:08 -0300

    feat(components): add Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,25 @@
+import { Component } from '../../types/Component';
+
+export const Modal: Component = () => {
+  return (
+    <>
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You have been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+              Yay!
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
