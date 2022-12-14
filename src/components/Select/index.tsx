9e7d1059c050e23529618d9071f3ba303172848e
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-14 11:56:17 -0300

    feat(components): add handleOnChange for Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -3,13 +3,17 @@ import { Component } from '../../types/Component';
 interface ISelect {
   title: string;
   options: string[];
+  handleOnChange: (...args: any[]) => void;
 }
 
 export const Select: Component<ISelect> = (props) => {
-  const { title, options } = props;
+  const { title, options, handleOnChange } = props;
 
   return (
-    <select className="select select-bordered w-full max-w-xs">
+    <select
+      className="select select-bordered w-full max-w-xs"
+      onChange={handleOnChange}
+    >
       <option disabled selected>
         {title}
       </option>
