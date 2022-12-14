aa19990387e2f1739af321428d7468e9bd5553d0
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-13 21:30:35 -0300

    feat(components): add new text-input

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -4,17 +4,28 @@ interface ITextInput {
   placeholder: string;
   value: string;
   handleTextInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: keyof typeof types;
 }
 
+const types = {
+  primary: 'input input-bordered w-full max-w-xs',
+  error: 'input input-bordered input-error w-full max-w-xs',
+  success: 'input input-bordered input-success w-full max-w-xs',
+};
+
 export const TextInput: Component<ITextInput> = (props) => {
-  const { placeholder, value, handleTextInput } = props;
+  const { placeholder, value, type: propsType, handleTextInput } = props;
+
+  const type = propsType || 'primary';
+
+  const defaultClassName = types[type];
 
   return (
     <input
       type="text"
       onChange={handleTextInput}
       placeholder={placeholder}
-      className="input input-bordered w-full max-w-xs"
+      className={defaultClassName}
       value={value}
     />
   );
