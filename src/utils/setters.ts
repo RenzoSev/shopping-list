afcd4badb2239c673cdab12a9bbc10f14d23a0a9
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-13 21:28:30 -0300

    feat(utils): add setters for global theme

--- src/utils/setters.ts
+++ src/utils/setters.ts
@@ -3,3 +3,12 @@ import { Avatar } from '../server/avatar';
 export function setAvatarInStorage(avatar: Avatar) {
   localStorage.setItem('avatar', JSON.stringify(avatar));
 }
+
+export function setGlobalThemeInStorage(theme: string) {
+  localStorage.setItem('theme', theme);
+}
+
+export function setGlobalDataThemeHTML(theme: string) {
+  const html = document.querySelector('html');
+  html?.setAttribute('data-theme', theme);
+}
