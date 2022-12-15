9c1057b7255c40bf3f460925d7be7a6490893b8c
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2022-12-15 00:00:15 -0300

    refactor(app): add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAvatar } from './hooks/useAvatar';
-import {
-  getAllProducts,
-  removeProduct,
-  createProduct,
-  Product,
-  updateProduct,
-} from './server';
+import { Home } from './pages/Home';
 
-function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const navigate = useNavigate();
-  const { hasAvatar } = useAvatar();
-
-  if (!hasAvatar) {
-    return navigate('/choose-avatar');
-  }
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      await getAllProducts(setProducts);
-    };
-
-    fetchProducts();
-  }, []);
-
-  function handleDeleteProduct(productId: string) {
-    removeProduct(productId);
-  }
-
-  function handleCreateProduct(product: Product) {
-    createProduct(product);
-  }
-
-  function handleUpdateProduct(product: Product) {
-    updateProduct(product);
-  }
-
-  return (
-    <div className="App">
-      <header></header>
-    </div>
-  );
+export function App() {
+  return <Home />;
 }
 
 export default App;
